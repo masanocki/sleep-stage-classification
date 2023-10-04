810fdc38cf57472c72ad336a1e92710866d53d4a
Maciej <76630179+HAHAHAHAHAXDDD@users.noreply.github.com>
2023-10-05 00:40:56 +0200

    nig

--- data_prep.py
+++ data_prep.py
@@ -4,7 +4,7 @@ import mne
 # data_type="raw" - raw data from mne reader
 # data_type="numb" - actual data in np.ndarray
 def read_file(filepath, data_type="raw"):
-    data = mne.io.read_raw_edf("datasets/SC4001E0-PSG.edf")
+    data = mne.io.read_raw_edf(filepath)
     if data_type == "numb":
         data = data.get_data()
     return data
--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import mne
 import matplotlib.pyplot as plt
 import data_prep as dp
 
-data = dp.read_file("datasets/SC4001E0-PSG.edf", data_type="raw")
-mne.viz.plot_raw(data)
+data = dp.read_file("datasets/recordings/SN001.edf", data_type="raw")
+mne.viz.plot_raw(data, block=True)
 plt.show()
